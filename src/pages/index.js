6d5217797d8321eed727fada80b1e149a4214e64
BrentRevelle <revellebrent@icloud.com>
2025-05-18 13:24:16 -0500

    api refactoring

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,6 @@
 import "./index.css";
 import { enableValidation, settings, resetValidation } from "../scripts/validation.js";
-import Api from "../scripts/Api.js";
-
-// const initialCards = [
-//   {
-//     name: "Golden Gate Bridge",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
-//   },
-//   {
-//     name: "Val Thorens",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
-//   },
-//   {
-//     name: "Restaurant terrace",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
-//   },
-//   {
-//     name: "An outdoor cafe",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
-//   },
-//   {
-//     name: "A very long bridge, over the forest and through the trees",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
-//   },
-//   {
-//     name: "Tunnel with morning light",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
-//   },
-//   {
-//     name: "Mountain house",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
-//   },
-// ];
+import Api from "../utils/Api.js";
 
 const api = new Api({
   baseUrl: "https://around-api.en.tripleten-services.com/v1",
@@ -41,10 +10,14 @@ const api = new Api({
   }
 });
 
-api.getInitialCards().then((initialCards) => {
-  console.log(initialCards);
-  initialCards.forEach((item) => renderCard(item, "append")); // work here
-});
+  api
+  .getAppInfo()
+  .then(([cards]) => {
+    console.log(cards);
+    cards.forEach((item) => renderCard(item, "append"));
+  })
+  .catch(console.error);
+
 
 const profileEditButton = document.querySelector(".profile__edit-button");
 const cardModalButton = document.querySelector(".profile__add-button");
---
+++ src/utils/Api.js
@@ -0,0 +1,25 @@
+class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  getAppInfo() {
+    return Promise.all([this.getInitialCards()]); //add initial request
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    }).then(res => {
+      if (res.ok) {
+        return res.json()
+      }
+      Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  // other methods for working with the API
+}
+
+export default Api;
